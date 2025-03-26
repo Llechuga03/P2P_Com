cf628eda1a3c6c70c02c5953fa56a7814555a22e
Llechuga03 <140262648+Llechuga03@users.noreply.github.com>
2025-03-25 21:17:41 -0400

    Was sedning too manya arguments to the remove_peer function, so fixed that

--- P2P.py
+++ P2P.py
@@ -36,19 +36,20 @@ def handle_peer(conn, addr):
 
 def broadcast(message, sender_conn):
     ''''Function that sends a message to all peers except the sender'''
-    for peer, addr in peers.items():
+    for peer in list(peers.keys()):
         if peer != sender_conn:
             try:
                 peer.send(message.encode())
             except:
-                remove_peer(peer, addr)
+                remove_peer(peer)
 
 def remove_peer(conn):
     ''''Function to remove a peer from the list of active connections'''
     if conn in peers:
+        addr = peers[conn]
         conn.close()
         del peers[conn]
-        print(f"Peer {peers[conn]} disconnected")
+        print(f"Peer {addr} disconnected")
 
 def connect_to_peer(peer_ip, peer_port):
     '''Function to connect to an existing peer'''
@@ -66,13 +67,16 @@ def connect_to_peer(peer_ip, peer_port):
 def user_input_handler():
     ''''Function to handle user input and commands'''
     while True:
-        message = input("(You): ")
+        message = input("(You): ").strip()
         if message.lower == "/exit":
             print("Exiting...")
             sys.exit(0)
         elif message.startswith("/connect"):
-            _, ip, port = message.split()
-            connect_to_peer(ip, int(port))
+            try:
+                _, ip, port = message.split()
+                connect_to_peer(ip, int(port))
+            except ValueError:
+                print("Invalid format. Use: /connect <IP> <PORT>")
         elif message.startswith("/peers"):
             print("Active peers:", list(peers.values()))
         elif message.startswith("/help"):
@@ -86,15 +90,15 @@ def user_input_handler():
 
 # Main function to start the server and handle user input
 if __name__ == "__main__":
-    print("Welcome to the P2P Chat System!")
+    print("Welcome to this basic P2P Chat System!")
     print("To connect to a peer, use the command: /connect <IP> <PORT>")
     print("To list active peers, use the command: /peers")
     print("To exit, type: /exit")
     print("To print this message again, type: /help")
     print("To send a message, just type it and hit enter.")
     while True:
-        if len(sys.argv) != 3 or not sys.argv[2].isdigit():
-            print("Usage: python3 p2p_chat.py <IP> <PORT>")
+        if len(sys.argv) != 3:
+            print("ERROR, invalid syntax. Please use the following syntax: python3 P2P.py <IP> <PORT>")
             sys.exit(1)
         try:
             ip = sys.argv[1]
@@ -102,6 +106,7 @@ if __name__ == "__main__":
             break
         except ValueError:
             print("Invalid arguments. Please enter a valid IP and PORT.")
+            sys.exit(1)
     
     threading.Thread(target=start_server, args=(ip, port), daemon=True).start()
     user_input_handler()
